903cfa5089e73e613956ac1b127579c5fb421d7b
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-11-29 11:51:04 +0900

    Wildcard import 제거

--- src/main/kotlin/study/securityjwt/config/JwtConfig.kt
+++ src/main/kotlin/study/securityjwt/config/JwtConfig.kt
@@ -6,7 +6,8 @@ import jakarta.annotation.PostConstruct
 import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.util.*
+import java.util.Base64
+import java.util.Date
 
 @Component
 @RequiredArgsConstructor
